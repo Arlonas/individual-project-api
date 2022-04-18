6b3446af86d2811bae7988414d1dd04553d70789
Arlonas <98306152+Arlonas@users.noreply.github.com>
2022-04-18 23:41:32 +0700

    feat: added get all post feature with pagination and limit post per page

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -3,6 +3,30 @@ const { Post } = require("../lib/sequelize");
 const PostControllers = {
     getAllPost: async (req, res, next) => {
         try {
+            const { _limit = 5, _page = 1  } = req.query
+        //  didelete karena biar g masuk ke dalam wherenya 
+        // biar g eror karena di dlm wherenya g ada kolom limit dan page
+            delete req.query._limit
+            delete req.query._page
+            const findPosts = await Post.findAllAndCountAll({
+              where: {
+                  ...req.query
+              },
+              _limit: _limit ? parseInt(_limit) :undefined,
+              offset: (_page - 1) * _limit
+            })
+
+            return res.status(200).json({
+                message: "Find posts",
+                result: findPosts
+            })
+        } catch (err) {
+            console.log(err)
+            next()
+        }
+    },
+    createPost: async (req, res, next) => {
+        try {
             const { image_url, location, caption } = req.body
             const newPost = await Post.create({
                image_url,
